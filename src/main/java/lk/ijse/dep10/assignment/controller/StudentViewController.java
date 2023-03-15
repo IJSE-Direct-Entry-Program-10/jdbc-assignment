b68dc60dc0e5eb68b9ab68cbc9490033923cbf23
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2023-03-15 12:12:07 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/dep10/assignment/controller/StudentViewController.java
@@ -0,0 +1,54 @@
+package lk.ijse.dep10.assignment.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class StudentViewController {
+
+    public Button btnDeleteStudent;
+    public Button btnNewStudent;
+    public Button btnSaveStudent;
+    public RadioButton rdFemale;
+    public RadioButton rdMale;
+    public TableView<?> tblStudents;
+    public ToggleGroup tglGender;
+    public TextField txtAddress;
+    public DatePicker txtDOB;
+    public TextField txtFirstName;
+    public TextField txtId;
+    public TextField txtLastName;
+    public AnchorPane root;
+    public Label lblId;
+    public Label lblFirstName;
+    public Label lblLastName;
+    public Label lblAddress;
+    public Label lblDOB;
+
+    public void initialize() {
+        lblId.setLabelFor(txtId);
+        lblFirstName.setLabelFor(txtFirstName);
+        lblLastName.setLabelFor(txtLastName);
+        lblAddress.setLabelFor(txtAddress);
+        lblDOB.setLabelFor(txtDOB);
+    }
+
+    public void btnDeleteStudentOnAction(ActionEvent event) {
+
+    }
+
+
+    public void btnNewStudentOnAction(ActionEvent event) {
+
+    }
+
+
+    public void btnSaveStudentOnAction(ActionEvent event) {
+
+    }
+
+    public void tblStudentsOnKeyReleased(KeyEvent keyEvent) {
+    }
+}
